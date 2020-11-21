e7dcbf77dff93c571ac675fdf6b4273c7d049f84
Jae Jin <jaej09@outlook.com>
2020-11-22 02:12:45 +0900

    Create habits component

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -1,21 +1,37 @@
 import React, { Component } from 'react';
 
 class Habit extends Component {
+  state = {
+    count : 0
+  };
+
+  handleIncrement = (evt) => {
+    // state안에 있는 count를 증가 한 뒤 state를 업데이트 해야 함
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  handleDecrement = (evt) => {
+    const count = this.state.count - 1;
+    this.setState({ count: count < 0 ? 0 : count });
+  };
+
   render() {
+    const { name, count } = this.props.habit;
+
     return (
-      <React.Fragment>
-        <span className="habit-name">Reading</span>
-        <span className="habit-count">8</span>
-        <button className="habit-button habit-increase">
+      <li className="habit">
+        <span className="habit-name">{name}</span>
+        <span className="habit-count">{count}</span>
+        <button className="habit-button habit-increase" onClick={this.handleIncrement}>
           <i className="fas fa-plus-square" />
         </button>
-        <button className="habit-button habit-decrease">
+        <button className="habit-button habit-decrease" onClick={this.handleDecrement}>
           <i className="fas fa-minus-square" />
         </button>
         <button className="habit-button habit-delete">
           <i className="fas fa-trash" />
         </button>
-      </React.Fragment>
+      </li>
     );
   }
 }
