8a5308a333281d98c8adcbe90793f4fd228dff05
Jae Jin <jaej09@outlook.com>
2020-11-22 22:39:51 +0900

    Handle events

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -1,20 +1,22 @@
 import React, { Component } from 'react';
 
 class Habit extends Component {
-  state = {
-    count : 0
+  handleIncrement = () => {
+    this.props.onIncrement(this.props.habit);
   };
 
-  handleIncrement = (evt) => {
-    // state안에 있는 count를 증가 한 뒤 state를 업데이트 해야 함
-    this.setState({ count: this.state.count + 1 });
+  handleDecrement = () => {
+    this.props.onDecrement(this.props.habit);
   };
 
-  handleDecrement = (evt) => {
-    const count = this.state.count - 1;
-    this.setState({ count: count < 0 ? 0 : count });
+  handleDelete = () => {
+    this.props.onDelete(this.props.habit);
   };
 
+  // render 함수는 데이터가 변경될 때 마다 계속해서 호출이 됨
+  // render 함수 내부에 있는 <button></button>에 직접 위에 있는 함수를 주게 되면
+  // onClick={ () => { this.props.onDelete } }
+  // render() 함수가 호출이 될 때 마다 함수가 생성이 된다는 단점이 있음
   render() {
     const { name, count } = this.props.habit;
 
@@ -22,13 +24,16 @@ class Habit extends Component {
       <li className="habit">
         <span className="habit-name">{name}</span>
         <span className="habit-count">{count}</span>
+
         <button className="habit-button habit-increase" onClick={this.handleIncrement}>
           <i className="fas fa-plus-square" />
         </button>
+
         <button className="habit-button habit-decrease" onClick={this.handleDecrement}>
           <i className="fas fa-minus-square" />
         </button>
-        <button className="habit-button habit-delete">
+
+        <button className="habit-button habit-delete" onClick={this.handleDelete}>
           <i className="fas fa-trash" />
         </button>
       </li>
--- src/components/habits.jsx
+++ src/components/habits.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+
 import Habit from './habit';
 
 class Habits extends Component {
@@ -9,8 +10,52 @@ class Habits extends Component {
       { id: 3, name: 'Coding', count: 0 }
     ]
   };
+
+  handleIncrement = (habit) => {
+    // console.log(habit);
+    const habits = [
+      ...this.state.habits // habits에 있는 object 복사 => state 내부에 정의한 habits가 그대로 복사됨
+    ];
+    // state에 있는 habits 데이터를 복사해온 이유는?
+    // state에 있는 데이터를 부분적으로 업데이트하면 리액트는 모른다.
+    // 즉, 데이터 전체를 업데이트해야 리액트가 알아차림
+    // this.state.habits[index].count++ (X) => 리액트가 알아차리지 못함
+
+    const index = habits.indexOf(habit); // habits 배열에서, 함수 인자로 넘어온 habit의 index는 몇번인지 찾기 [0, 1, 2 ...]
+    habits[index].count++;
+
+    this.setState({ habits }); // 여기서 데이터 전체를 업데이트 함
+  };
+
+  handleDecrement = (habit) => {
+    const habits = [
+      ...this.state.habits
+    ];
+    const index = habits.indexOf(habit);
+    const count = habits[index].count--;
+    habits[index].count = count < 0 ? 0 : count; // count 값이 0 보다 작으면 habits[index].count = 0으로 설정
+    this.setState({ habits });
+  };
+
+  handleDelete = (habit) => {
+    const habits = this.state.habits.filter((item) => item.id !== habit.id); // 함수 인자로 넘어온 habit만 제외하고 변수에 포함
+    this.setState({ habits });
+  };
+
   render() {
-    return <ul>{this.state.habits.map((habit) => <Habit key={habit.id} habit={habit} />)}</ul>;
+    return (
+      <ul>
+        {this.state.habits.map((habit) => (
+          <Habit
+            key={habit.id}
+            habit={habit}
+            onIncrement={this.handleIncrement}
+            onDecrement={this.handleDecrement}
+            onDelete={this.handleDelete}
+          />
+        ))}
+      </ul>
+    );
   }
 }
 
